45b72fa2bc7a08ffa7223bf3566e11d41c7f6b16
sr-sourav-roy <sourav.cp.bd@gmail.com>
2026-03-24 07:11:27 +0600

    add nav_ber done

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,45 @@
+// function loadCategory() {
+//   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+//     .then((res) => res.json())
+//     .then((data) => displayCategory(data.categories));
+// }
+
+// // API this call:-
+// //             "category_id": "1001",
+// //             "category": "Music"
+// //         },
+
+// function displayCategory(categories) {
+//   for (const cate of categories) {
+//     const CategoryContainer = document.getElementById("Category-container");
+
+//     const categoryDiv = document.createElement("div");
+//     categoryDiv.innerHTML = `
+//       <button class="btn btn-sm hover:bg-[#FF1F3D]">${cate.category}</button>
+//       `;
+//     CategoryContainer.append(categoryDiv);
+//   }
+// }
+
+// loadCategory();
+
 function loadCategory() {
-  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories#")
     .then((res) => res.json())
     .then((data) => displayCategory(data.categories));
 }
-function displayCategory(categories) {
-  console.log(categories);
+function displayCategory(typeNames) {
+  for (const name of typeNames) {
+    // get to the
+    const CategoryContainer = document.getElementById("Category-container");
+
+    //   created
+    const categoryDiv = document.createElement("div");
+    categoryDiv.innerHTML = `
+      <button class="btn btn-sm  hover:bg-[#FF1F3D] p-4">${name.category}</button>
+      `;
+    CategoryContainer.append(categoryDiv);
+  }
 }
 
 loadCategory();
