aad737133f077c4e8f324e965b70ffc2222ea81a
sr-sourav-roy <sourav.cp.bd@gmail.com>
2026-03-26 10:05:05 +0600

    add by button value

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,65 @@
-// loadCategory functions:
+// removes active class:
+function activeRemoveClass() {
+  const removeActiveClass = document.getElementsByClassName("active");
+  for (let bttn of removeActiveClass) {
+    bttn.classList.remove("active");
+  }
+}
+
+//😊 loadCategory functions:
 function loadCategory() {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories#")
     .then((res) => res.json())
     .then((data) => displayCategory(data.categories));
 }
-// loadVideos function:
+
+// 😊 loadVideos function:
 function loadVideo() {
   fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
     .then((res) => res.json())
-    .then((data) => displayVideo(data.videos));
+    .then((data) => {
+      activeRemoveClass();
+      document.getElementById("bttn-all").classList.add("active");
+      displayVideo(data.videos);
+    });
 }
-// loadCategoryVideo:d
+// 😊 loadCategoryVideo:d
 const loadCategoryVideos = (id) => {
-  const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
-  console.log(url);
-  fetch(url)
+  const ulr = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
+  console.log(ulr);
+  fetch(ulr)
     .then((res) => res.json())
-    .then((data) => displayVideo(data.category));
+    .then((data) => {
+      // call this removed actives functions:
+      activeRemoveClass();
+
+      // dynamical created btn:
+      const clickButton = document.getElementById(`btn-${id}`);
+
+      // add styles items:
+      clickButton.classList.add("active");
+
+      // call the object and array:
+      displayVideo(data.category);
+    });
 };
 
-// displayCategory function:
+// ☠️ displayCategory function:
 function displayCategory(typeNames) {
   for (const name of typeNames) {
     // get to the
     const CategoryContainer = document.getElementById("Category-container");
 
-    //   created
+    //   created category btn of 3 dynamical
     const categoryDiv = document.createElement("div");
     categoryDiv.innerHTML = `
-      <button onclick="loadCategoryVideos(${name.category_id})" class="btn btn-sm  hover:bg-[#FF1F3D] p-4">${name.category}</button>
+      <button id="btn-${name.category_id}" onclick="loadCategoryVideos(${name.category_id})" class="btn btn-sm  hover:bg-[#FF1F3D] p-4">${name.category}</button>
       `;
     CategoryContainer.append(categoryDiv);
   }
 }
-//displayVideo function:
+
+// ☠️ displayVideo function:
 
 // "category_id": "1001",
 // "video_id": "aaaa",
@@ -51,11 +77,27 @@ function displayCategory(typeNames) {
 // "posted_date": "16278"
 // },
 // "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey
-
 const displayVideo = (videos) => {
   const videoContainer = document.getElementById("video-container");
+
   // this point--> finish hum all videos
   videoContainer.innerHTML = "";
+
+  // random call function:
+  if (videos.length == 0) {
+    videoContainer.innerHTML = `
+     <div
+        class="col-span-full flex justify-center items-center flex-col py-20"
+      >
+        <img class="w-[200px]" src="./assests/Icon.png" alt="" />
+        <h2 class="text-3xl font-bold">
+          Oops!! Sorry, There is no content here
+        </h2>
+      </div>
+    `;
+    return;
+  }
+
   videos.forEach((video) => {
     const videoCard = document.createElement("div");
     videoCard.innerHTML = `
@@ -98,5 +140,5 @@ const displayVideo = (videos) => {
   });
 };
 
-// call functions:
+// 🔥call functions:
 loadCategory();
